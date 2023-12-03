d5e1b46ebd12f17116967605f4cbe7387ec49ef5
Yackie Burch <jaclynnburch@gmail.com>
2023-12-03 14:09:35 -0800

    add questions

--- Develop/index.js
+++ Develop/index.js
@@ -7,8 +7,42 @@ const generateMarkdown = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
 const questions = [
      {
-        type: 
-     }
+        type: 'input',
+        name: 'title',
+        message: 'Your Project Title',
+     },
+     {
+        type: 'input',
+        name: 'description',
+        message: 'A short description of the purpose and functionality of your project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Steps required to install your project',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide instruction and examples for use, including screenshots. To add a screenshot, create an \'assets/images\' folder in your repot to upload your files to.'
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'List your collaborators, with links to their GitHub profiles. For other resouces, please provide links'
+    },
+    {
+        type: 'checkbox',
+        name: 'license',
+        message: 'Please select a license application to this project',
+        choices: ['MIT', 'Apache 2.0', 'GNU', 'Creative Commons Licesnses'],
+    },
+    {
+        type: 'input',
+        name: 'features',
+        message: 'Please list features, if not mentioned above.',
+    },
+    
 ];
 
 // TODO: Create a function to write README file
